68337d46eb5469ae795b1710fe19dd04c11ba148
Mariana Leite <marianacoleite@gmail.com>
2025-11-26 11:42:34 -0300

    Node.js
    Estruturação para o node.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Configuração do EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+// Public (CSS/JS/IMG)
+app.use(express.static(path.join(__dirname, "src/public")));
+
+// ROTA PRINCIPAL
+app.get("/", (req, res) => {
+    res.render("site"); // dashboard.ejs
+});
+
+// Porta
+const port = 3000;
+app.listen(port, () => console.log(`🚀 Servidor rodando http://localhost:${port}`));
